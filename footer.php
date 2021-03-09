fa5a09c5fa1aa4d7436da1e42581e5ef88e0c057
guisopo <solerguillermo@hotmail.com>
2021-03-09 18:09:03 +0100

    Add comments

--- footer.php
+++ footer.php
@@ -1,7 +1,20 @@
-<footer>
-    <section class="footer-widgets">Footer Widgets</section>
-    <section class="copyright">copyright</section>
-  </footer>
-</div>
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Lamateria
+ */
+
+?>
+    <footer>
+      <section class="footer-widgets">Footer Widgets</section>
+      <section class="copyright">copyright</section>
+    </footer>
+  </div><!-- #page -->
+  
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Lamateria
+ */
+
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
