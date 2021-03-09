b8b51777659c75810f546ee652925dee980f1628
guisopo <solerguillermo@hotmail.com>
2021-03-09 17:59:03 +0100

    Add basic html structure

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lamateria</title>
+</head>
+<body <?php body_class($class); ?>>
+  <div id="page" class="site">
+    <header>
+      <section class="search">Search</section>
+      <section class="top-bar">
+        <div class="brand">Logo</div>
+        <div class="second-column">
+          <div class="account">Account</div>
+          <div class="main-menu">Menu</div>
+        </div>
+      </section>
+    </header>
+    <div class="content-area">
+      <main>
+        <section class="slider">Slider</section>
+        <section class="popular-products">Popular Products</section>
+        <section class="new-arrivals">Deal of the week</section>
+        <section class="blog">News</section>
+      </main>
+    </div>
+    <footer>
+      <section class="footer-widgets">Footer Widgets</section>
+      <section class="copyright">copyright</section>
+    </footer>
+  </div>
+</body>
+</html>
