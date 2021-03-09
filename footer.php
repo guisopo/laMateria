d81bdd695a775d43dda18036591d71379208c4ef
guisopo <solerguillermo@hotmail.com>
2021-03-09 18:05:17 +0100

    Move header and footer to new files and import them to index

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <section class="footer-widgets">Footer Widgets</section>
+    <section class="copyright">copyright</section>
+  </footer>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' );  ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body <?php body_class($class); ?>>
+  <div id="page" class="site">
+    <header>
+      <section class="search">Search</section>
+      <section class="top-bar">
+        <div class="brand">Logo</div>
+        <div class="second-column">
+          <div class="account">Account</div>
+          <div class="main-menu">Menu</div>
+        </div>
+      </section>
+    </header>
