af96d2ba4075db37ab0425406226aac1358beffa
guisopo <solerguillermo@hotmail.com>
2021-03-11 13:02:28 +0100

    Reorganize files

--- functions.php
+++ functions.php
@@ -18,8 +18,8 @@
  */
 function lamateria_scripts() {
   wp_enqueue_style( 'lamateria-style', get_stylesheet_uri(), array(), '1.0', 'all' );
-  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '4.6.0', 'all' );
-  wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array( 'jquery' ), '4.3.1', true);
+  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/bootstrap/bootstrap.min.css', array(), '4.6.0', 'all' );
+  wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/bootstrap/bootstrap.min.js', array( 'jquery' ), '4.3.1', true);
 }
 
 add_action( 'wp_enqueue_scripts', 'lamateria_scripts' );
