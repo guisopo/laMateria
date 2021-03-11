5ac8b08740a6e0fa67e421edc589d2086595caac
guisopo <solerguillermo@hotmail.com>
2021-03-11 18:43:35 +0100

    Show some modifications only if we are in the shop area

--- inc/wc-modifications.php
+++ inc/wc-modifications.php
@@ -1,53 +1,65 @@
 <?php 
+/**
+ * Template Overrides for WooCommerce pages
+ *
+ * @link https://docs.woocommerce.com/document/woocommerce-theme-developer-handbook/#section-3
+ *
+ * @package Fancy Lab
+ */
 
-/** 
-* Modify WooCommerce opening and closing HTML tags
-* We need Bootstrap-like opening/closing HTML tags
-*/
-add_action( 'woocommerce_before_main_content', 'fancy_lab_open_container_row', 5 );
-function fancy_lab_open_container_row(){
-	echo '<div class="container shop-content"><div class="row">';
+function lamateria_wc_modify(){
+	/** 
+	* Modify WooCommerce opening and closing HTML tags
+	* We need Bootstrap-like opening/closing HTML tags
+	*/
+	add_action( 'woocommerce_before_main_content', 'lamateria_open_container_row', 5 );
+	function lamateria_open_container_row(){
+		echo '<div class="container shop-content"><div class="row">';
+	}
+
+	add_action( 'woocommerce_after_main_content', 'lamateria_close_container_row', 5 );
+	function lamateria_close_container_row(){
+		echo '</div></div>';
+	}
+
+	/** 
+	* Remove the main WC sidebar from its original position
+	* We'll be including it somewhere else later on
+	*/
+	remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar' );
+
+	if( is_shop() ){
+
+		add_action( 'woocommerce_before_main_content', 'lamateria_add_sidebar_tags', 6 );
+		function lamateria_add_sidebar_tags(){
+			echo '<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">';
+		}
+
+		// Put the main WC sidebar back, but using other action hook and on a different position
+		add_action( 'woocommerce_before_main_content', 'woocommerce_get_sidebar', 7 );
+
+		add_action( 'woocommerce_before_main_content', 'lamateria_close_sidebar_tags', 8 );
+		function lamateria_close_sidebar_tags(){
+			echo '</div>';
+		}
+		// Also, if we are on a shop page, include the product description
+		add_action( 'woocommerce_after_shop_loop_item_title', 'the_excerpt', 1 );		
+	}
+
+
+	// Modify HTML tags on a shop page. We also want Bootstrap-like markup here (.primary div)
+	add_action( 'woocommerce_before_main_content', 'lamateria_add_shop_tags', 9 );
+	function lamateria_add_shop_tags(){
+		if( is_shop()){
+			echo '<div class="col-lg-9 col-md-8 order-1 order-md-2">';
+		} else{
+			echo '<div class="col">';
+		}
+	}
+	add_action( 'woocommerce_after_main_content', 'lamateria_close_shop_tags', 4 );
+	function lamateria_close_shop_tags(){
+		echo '</div>';
+	}
+		
 }
-
-add_action( 'woocommerce_after_main_content', 'fancy_lab_close_container_row', 5 );
-function fancy_lab_close_container_row(){
-	echo '</div></div>';
-}
-
-add_action( 'woocommerce_before_main_content', 'fancy_lab_add_sidebar_tags', 6 );
-function fancy_lab_add_sidebar_tags(){
-	echo '<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">';
-}
-
-/** 
-* Remove the main WC sidebar from its original position
-* We'll be including it somewhere else later on
-*/
-remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar' );
-
-// Put the main WC sidebar back, but using other action hook and on a different position
-add_action( 'woocommerce_before_main_content', 'woocommerce_get_sidebar', 7 );
-
-add_action( 'woocommerce_before_main_content', 'fancy_lab_close_sidebar_tags', 8 );
-function fancy_lab_close_sidebar_tags(){
-	echo '</div>';
-}
-
-// Modify HTML tags on a shop page. We also want Bootstrap-like markup here (.primary div)
-add_action( 'woocommerce_before_main_content', 'fancy_lab_add_shop_tags', 9 );
-function fancy_lab_add_shop_tags(){
-	echo '<div class="col-lg-9 col-md-8 order-1 order-md-2">';
-}
-
-add_action( 'woocommerce_after_main_content', 'fancy_lab_close_shop_tags', 4 );
-function fancy_lab_close_shop_tags(){
-	echo '</div>';
-}
-
-/*add_filter( 'woocommerce_show_page_title', 'fancy_lab_remove_shop_title' );
-function fancy_lab_remove_shop_title( $val ){
-	$val = false;
-	return $val;
-} */
-
-add_action( 'woocommerce_after_shop_loop_item_title', 'the_excerpt', 1 );
+add_action( 'wp', 'lamateria_wc_modify' );
--- page.php
+++ page.php
@@ -20,7 +20,7 @@
             // Load them
             while ( have_posts() ) : the_post();
             ?>
-              <article>
+              <article class="col">
                 <h1><?php the_title(); ?></h1>
                 <div><?php the_content(); ?></div>
               </article>
