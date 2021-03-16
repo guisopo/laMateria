beefd952e44878a0d319f4ef9c88a11cef9b7bfa
guisopo <solerguillermo@hotmail.com>
2021-03-16 12:13:19 +0100

    Create new fields to poblate slider in customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -22,13 +22,165 @@ function lamateria_customizer( $wp_customize ) {
       'sanitize_callback' => 'sanitize_text_field'
     )
   );
-  // Field 1 - Copyright Text Box
+  // Field 2 - Copyright Text Box
   $wp_customize->add_control(
     'set_copyright', array(
       'label'       => 'Copyright',
       'description' => 'Please, add your copyright information here',
       'section'     => 'section_copyright',
       'type'        => 'text'
+    )
+  );
+  //*---------------------------------------
+  // Slider Section
+  $wp_customize->add_section(
+    'section_slider', array(
+      'title'       => 'Slider Settings',
+      'description' => 'Slider Section'
+    )
+  );
+  // Field 1 - Slider Page Number 1
+  $wp_customize->add_setting(
+    'set_slider_1', array(
+      'type'              => 'theme_mod',
+      'default'           => '',
+      'sanitize_callback' => 'absint',
+    )
+  );
+  $wp_customize->add_control(
+    'set_slider_1', array(
+      'label'       => 'Set slider 1',
+      'description' => 'Set slider 1',
+      'section'     => 'section_slider',
+      'type'        => 'dropdown-pages'
+    )
+  );
+  // Field 2 - Slider Button Text 1
+  $wp_customize->add_setting(
+    'set_slider_button_text_1', array(
+      'type'              => 'theme_mod',
+      'default'           => '',
+      'sanitize_callback' => 'sanitize_text_field',
+    )
+  );
+  $wp_customize->add_control(
+    'set_slider_button_text_1', array(
+      'label'       => 'Button text for slider 1',
+      'description' => 'Button text for slider 1',
+      'section'     => 'section_slider',
+      'type'        => 'text'
+    )
+  );
+  // Field 3 - Slider Button Url 1
+  $wp_customize->add_setting(
+    'set_slider_button_url_1', array(
+      'type'              => 'theme_mod',
+      'default'           => '',
+      'sanitize_callback' => 'esc_url_raw',
+    )
+  );
+  $wp_customize->add_control(
+    'set_slider_button_url_1', array(
+      'label'       => 'URL text for slider 1',
+      'description' => 'URL text for slider 1',
+      'section'     => 'section_slider',
+      'type'        => 'url'
+    )
+  );
+  // Field 1 - Slider Page Number 2
+  $wp_customize->add_setting(
+    'set_slider_2', array(
+      'type'              => 'theme_mod',
+      'default'           => '',
+      'sanitize_callback' => 'absint',
+    )
+  );
+  $wp_customize->add_control(
+    'set_slider_2', array(
+      'label'       => 'Set slider 2',
+      'description' => 'Set slider 2',
+      'section'     => 'section_slider',
+      'type'        => 'dropdown-pages'
+    )
+  );
+  // Field 2 - Slider Button Text 2
+  $wp_customize->add_setting(
+    'set_slider_button_text_2', array(
+      'type'              => 'theme_mod',
+      'default'           => '',
+      'sanitize_callback' => 'sanitize_text_field',
+    )
+  );
+  $wp_customize->add_control(
+    'set_slider_button_text_2', array(
+      'label'       => 'Button text for slider 2',
+      'description' => 'Button text for slider 2',
+      'section'     => 'section_slider',
+      'type'        => 'text'
+    )
+  );
+  // Field 3 - Slider Button Url 2
+  $wp_customize->add_setting(
+    'set_slider_button_url_2', array(
+      'type'              => 'theme_mod',
+      'default'           => '',
+      'sanitize_callback' => 'esc_url_raw',
+    )
+  );
+  $wp_customize->add_control(
+    'set_slider_button_url_2', array(
+      'label'       => 'URL text for slider 2',
+      'description' => 'URL text for slider 2',
+      'section'     => 'section_slider',
+      'type'        => 'url'
+    )
+  );
+  // Field 1 - Slider Page Number 3
+  $wp_customize->add_setting(
+    'set_slider_3', array(
+      'type'              => 'theme_mod',
+      'default'           => '',
+      'sanitize_callback' => 'absint',
+    )
+  );
+  $wp_customize->add_control(
+    'set_slider_3', array(
+      'label'       => 'Set slider 3',
+      'description' => 'Set slider 3',
+      'section'     => 'section_slider',
+      'type'        => 'dropdown-pages'
+    )
+  );
+  // Field 2 - Slider Button Text 3
+  $wp_customize->add_setting(
+    'set_slider_button_text_3', array(
+      'type'              => 'theme_mod',
+      'default'           => '',
+      'sanitize_callback' => 'sanitize_text_field',
+    )
+  );
+  $wp_customize->add_control(
+    'set_slider_button_text_3', array(
+      'label'       => 'Button text for slider 3',
+      'description' => 'Button text for slider 3',
+      'section'     => 'section_slider',
+      'type'        => 'text'
+    )
+  );
+  // Field 3 - Slider Button Url 3
+  $wp_customize->add_setting(
+    'set_slider_button_url_3', array(
+      'type'              => 'theme_mod',
+      'default'           => '',
+      'sanitize_callback' => 'esc_url_raw',
+    )
+  );
+  $wp_customize->add_control(
+    'set_slider_button_url_3', array(
+      'label'       => 'URL text for slider 3',
+      'description' => 'URL text for slider 3',
+      'section'     => 'section_slider',
+      'type'        => 'url'
     )
   );
 }
