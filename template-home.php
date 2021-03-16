7ce1c0f50ed109ca2afdd4e616957da2375c215c
guisopo <solerguillermo@hotmail.com>
2021-03-16 12:58:11 +0100

    Create slider in template

--- template-home.php
+++ template-home.php
@@ -11,18 +11,34 @@ get_header();
     <section class="slider">
       <div class="flexslider">
         <ul class="slides">
-          <li>
-            <img src="slide1.jpg" />
-          </li>
-          <li>
-            <img src="slide2.jpg" />
-          </li>
-          <li>
-            <img src="slide3.jpg" />
-          </li>
-          <li>
-            <img src="slide4.jpg" />
-          </li>
+          <?php
+            // Getting data from Customizer to display the Slider section
+            for ($i=1; $i < 4; $i++) { 
+              $slider_page[$i] = get_theme_mod('set_slider_' . $i);
+              $slider_button_text[$i] = get_theme_mod('set_slider_button_text_' . $i);
+              $slider_button_url[$i] = get_theme_mod('set_slider_button_url_' . $i);
+            }
+            $args = array(
+              'post_type'     =>  'page',
+              'post_per_page' =>  3,
+              'post__in'      =>  $slider_page,
+              'orderby'       =>  'post__in'
+            );
+
+            $slider_loop = new WP_Query($args);
+
+            if( $slider_loop -> have_posts() ) :
+              while( $slider_loop -> have_posts() ) : $slider_loop -> the_post();
+              ?>
+                <li>
+                hola
+                  <?php the_post_thumbnail('lamateria_slider', array( 'class' => 'img-fluid' ) ); ?>
+                </li>
+              <?php
+              endwhile;
+              wp_reset_postdata();
+            endif;
+          ?>
         </ul>
       </div>
     </section>
