4035fe7eef98e661f7e301e9c953a923b32ca88a
guisopo <solerguillermo@hotmail.com>
2021-03-17 12:46:21 +0100

    Improve index structure

--- index.php
+++ index.php
@@ -17,25 +17,45 @@
 
 <div class="content-area">
   <main>
-    <div class="row">
-      <?php 
-        // I there are any post
-        if( have_posts() ):
-          // Load them
-          while ( have_posts() ) : the_post();
-          ?>
-            <article>
-              <h2><?php the_title(); ?></h2>
-              <div><?php the_content(); ?></div>
-            </article>
-          <?php
-          endwhile;
-        else:
-          ?>
-            <p>Nothing to display.</p>
-          <?php
-        endif;
-      ?>
+    <div class="container">
+      <div class="row">
+        <?php 
+          // I there are any post
+          if( have_posts() ):
+            // Load them
+            while ( have_posts() ) : the_post();
+            ?>
+              <article <?php post_class(); ?>>
+                <h2>
+                  <a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a>
+                </h2>
+                <div class="meta">
+                    <p>Published by <?php echo get_the_author_posts_link(); ?> | on <?php echo get_the_date(); ?></p>
+                    <?php if( has_category() ) : ?>
+                      <p>Categories: <span><?php the_category( ',' ); ?></span></p>
+                    <?php endif; ?>
+                    <?php if( has_tag() ) : ?>
+                      <p>Tags: <span><?php the_tags('', ','); ?></span></p>
+                    <?php endif; ?>
+                </div>
+                <div class="post-thumbnail">
+                  <?php 
+                    if( has_post_thumbnail() ) :
+                      the_post_thumbnail( 'lamateria_blog', array( 'class' => 'img-fluid' ) );
+                    endif; 
+                  ?>
+                </div>
+                <div><?php the_excerpt(); ?></div>
+              </article>
+            <?php
+            endwhile;
+          else:
+            ?>
+              <p>Nothing to display.</p>
+            <?php
+          endif;
+        ?>
+      </div>
     </div>
   </main>
 </div>
